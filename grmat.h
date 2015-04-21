13959cec751d7c4b4307b6ad715186be0c997d9c
ywang85 <ywang85@slu.edu>
2015-04-21 16:47:19 -0500

    gdf format deserialize

--- grmat.h
+++ grmat.h
@@ -10,6 +10,7 @@
 #define __Graphm__
 
 #include <stdio.h>
+#include <iostream>
 #include <ctype.h>
 #include <string>
 
@@ -88,23 +89,58 @@ public:
   int getMark(int v) { return mark[v]; }
   void setMark(int v, int val) { mark[v] = val; }
 	
-	void serialize(ostream& o) const{
-		o<<"graph serializedGraph {"
-		for (int i=0;i<numVert;i++){
-			for (int j=;j<numVert;j++){
-				if (matrix[i][j]!=0){
-					o<<"v"<<i<<" -> "<<"v"<<j<<" "<<"[label="<<matrix[i][j]<<"];"<<endl;
-				}	
+	void serialize(ostream& o) /*const*/{
+		o<<"digraph serializedGraph {"<<endl;
+		for (int i=0;i<n();i++){
+			for (int j = first(i); j != n(); j = next(i, j)) {
+					o<<"\tv"<<i<<" -> "<<"v"<<j<<" [label="<<weight(i, j)<<"];"<<endl;
 			}
 		}
-	o<<"}";
+	o<<"}"<<endl;
   }
+
 	
 	void deserialize(istream& i){
 		string line;
-		getline(i, line);
-		while (getline(i,line)) {
-			cout << line << endl;
+		string vertexConvention = "v";
+		string delimiter = " ";
+		getline(i, line); // header
+		while (getline(i,line) && line.find("}") == string::npos) {
+			unsigned long vPos = 0;
+			unsigned long spPos = 0;
+			int data[3] = {0,0};
+			for (int i = 0; i < 2; i++) {
+				vPos = line.find(vertexConvention,vPos)+1;//从0开始找v位置，vPos=位置+1 => e.g. v1的1的位置
+				spPos = line.find(delimiter,spPos);//找空格的位置
+				string stringVertex = line.substr(vPos, spPos-vPos);//substring [vPos,spPos) v1的1
+				data[i] = stoi(stringVertex);//string转换成int e.g. data=[1]
+			}
+			unsigned long eqPos = line.find("=")+1;
+			string weight = line.substr(eqPos,line.find("]")-eqPos);
+			data[2] = stoi(weight);
+			setEdge(data[0], data[1], data[2]);
+		}
+	}
+	
+	void GDFdeserialize(istream& i){
+		string line;
+		string vertex="v";
+		string delimiter=",";
+		for (int j=0;j<5;j++)
+			getline(i,line);
+		while (getline(i,line)){
+			unsigned long vPos=0;
+			unsigned long cmPos=0;
+			int data[3];
+			for (int j=0;j<2;j++){
+				vPos=line.find(vertex,vPos)+1;
+				cmPos=line.find(delimiter,cmPos;
+				string stringVertex=line.substr(vPos,cmPos-vPos);
+				data[i]=stoi(stringVertex);
+			}
+			string weight=line.substr(cmPos+1,-1);
+			data[2]=stoi(weight);
+			setEdge(data[0],data[1],data[2]);
 		}
 	}
 	
