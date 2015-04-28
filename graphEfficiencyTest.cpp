0f798c13e4c03f1e805d637195c104ea7c8f93dd
cwlucas41 <cwlucas41@gmail.com>
2015-04-27 23:42:58 -0500

    Monday work

--- graphEfficiencyTest.cpp
+++ graphEfficiencyTest.cpp
@@ -7,3 +7,35 @@
 //
 
 #include <stdio.h>
+#include "Functions.h"
+#include "grmat.h"
+#include "grlist.h"
+#include "graphutil.h"
+#include "book.h"
+#include "dijkstra.h"
+
+
+int main(int argc, const char * argv[]) {
+	
+	int size = 5000;
+	int dijkList[size];
+	for (int vertexToCheck = 0; vertexToCheck < size; vertexToCheck++) {
+		Graphl G(size);
+
+		for (int i = 0; i < size; i++) {
+		dijkList[i] = INFINITY;
+		}
+		dijkList[vertexToCheck]=0;
+	
+		Settime();
+		Dijkstra(&G, dijkList, vertexToCheck);
+		cout << Gettime() << endl;
+	}
+	
+	Graphl listGraph(size);
+	Graphm matGraph(size);
+	cout<<"size of list implementation: "<<sizeof(listGraph)<<endl<<"size of matrix implementation: "<<sizeof(matGraph)<<endl;
+	
+	
+	return 0;
+}
